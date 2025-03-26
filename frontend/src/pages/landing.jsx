f2e7310eeb1d2b314b84fd24a92fe24bc85c1a08
diksha muktan <dikshamuktan966@gmail.com>
2025-03-26 17:35:24 +0545

    landing page update

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,10 +1,29 @@
+import { Github, Instagram, Mail } from "lucide-react";
+import Slider from "../components/ui/slider";
 export function LandingPage() {
   return (
-    <div className="flex justify-center">
-      <div className="smallContainer rounded-md border border-gray-600">
-        <div className="flex h-3/4 gap-6 mt-3 ml-3">
+    <div className="flex flex-col items-center gap-11 justify-center">
+      <div className="smallContainer p-3 rounded-lg border border-gray-600">
+        <div className="flex justify-end gap-3">
+          <a
+            className="border border-gray-600 p-1 rounded-md"
+            href="https://github.com/dikshamuktan?tab=overview&from=2024-12-01&to=2024-12-31"
+          >
+            <Github />
+          </a>
+          <a className="border border-gray-600 p-1 rounded-md" href="">
+            <Mail />
+          </a>
+          <a
+            className="border border-gray-600 p-1 rounded-md"
+            href="https://www.instagram.com/l.diksha/"
+          >
+            <Instagram />
+          </a>
+        </div>
+        <div className="flex h-3/5 gap-6 mt-11 px-3 ">
           <img
-            className="w-2/5 h-2/4 object-cover rounded-lg "
+            className="object-cover rounded-lg "
             src="https://images.pexels.com/photos/31289592/pexels-photo-31289592/free-photo-of-young-woman-in-nature-with-bright-foliage.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
           />
           <div className="flex flex-col p-2">
@@ -12,6 +31,9 @@ export function LandingPage() {
             <p className="text-2xl">I'm a Full Stack Developer</p>
           </div>
         </div>
+      </div>
+      <div>
+        <Slider />
       </div>
     </div>
   );
