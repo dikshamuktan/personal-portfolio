7b43243c5bd7cf11b3b7e511addb1fe3bfba8643
diksha muktan <dikshamuktan966@gmail.com>
2025-04-12 21:57:42 +0545

    contact page updaate

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -2,8 +2,11 @@ export function AboutPage() {
   return (
     <div className="flex flex-col items-center">
       <h1 className="stylefont text-5xl font-bold mt-11">About Me</h1>
-      <p></p>
-      <p></p>
+      <h1>I am a Full Stack Developer</h1>
+      <p>
+        I have a strong command over both front-end and back-end technologies,
+        allowing me to build complete and scalable applications.
+      </p>
     </div>
   );
 }
--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -14,9 +14,33 @@ export function ContactPage() {
           Contact Me
         </h1>
         <ToastContainer />
-        <button className="absolute z-10 bottom-10" onClick={notify}>
-          Notify!
-        </button>
+        <form className="mt-26  flex flex-col gap-3 w-2/4">
+          <label className="rounded-md  p-2">
+            Your Name
+            <br />
+            <input type="text" placeholder="Name...." />
+          </label>
+          <label className="rounded-md  p-2">
+            Your Email
+            <br />
+            <input type="text" placeholder="Email...." />
+          </label>
+          <label className="rounded-md  p-2">
+            Contact Number
+            <br />
+            <input type="text" placeholder="Number...." />
+          </label>
+          <label className="rounded-md  p-2">
+            Message
+            <br />
+            <textarea type="text" placeholder="Enter your message...." />
+          </label>
+          <label className="flex text-center">
+            <button className="" onClick={notify}>
+              Notify!
+            </button>
+          </label>
+        </form>
       </div>
     </div>
   );
